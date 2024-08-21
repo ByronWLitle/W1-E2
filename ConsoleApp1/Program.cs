cd3c4ff2fdfcb79ff89847187e75ef3fdada0df9
ByronWLitle <bwlitle@amarillocollege.com>
2024-08-21 13:38:14 -0500

    Comments added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,17 +11,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your first name?");
-            string firstname = Console.ReadLine();
-            
-            Console.WriteLine("What is your last name?");
-            string lastname = Console.ReadLine();
+            //Prompts user to input first name
+            Console.Write("What is your first name? ");
+            //Declare a variable firstname to hold the first name from user
+            string firstname = string.Empty;
+            //Assign the user input string to firstname variable
+            firstname = Console.ReadLine();
 
-            Console.WriteLine("What is your age?");
-            string age = Console.ReadLine();
+            //Prompts user to input last name
+            Console.Write("What is your last name? ");
+            //Declare a variable last to hold the last name from user
+            string lastname = string.Empty;
+            //Assign the user input string to lastname variable
+            lastname = Console.ReadLine();
 
-            Console.WriteLine("What is your favorite number?");
-            string favnum = Console.ReadLine();
+            //Prompts user to input age
+            Console.Write("What is your age? ");
+            //Declare a variable age to hold the age from user
+            string age = string.Empty;
+            //Assign the user input string to age variable
+            age = Console.ReadLine();
+
+            //Prompts user to input favorite number
+            Console.Write("What is your favorite number? ");
+            //Declare a variable favnum to hold the favorite number from user
+            string favnum = string.Empty;
+            //Assign the user input string to firstname variable
+            favnum = Console.ReadLine();
 
             Console.WriteLine("Hello "+ firstname + " "+ lastname + "! You are "+ age + " years old and your favorite number is " + favnum +".");
 
